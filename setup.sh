0574874cd285a2c7ef0d177f147d5b31de05e7d2
Carambole <131371657+MrCarambole@users.noreply.github.com>
2024-06-20 11:09:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ read -p "Interface à utiliser: " inter
 sed -i "s/INTERFACE/$inter/g" /lib/systemd/system/snort.service
 sudo systemctl daemon-reload
 sudo systemctl start snort
+sudo chmod 755 /var/log/snort
+sudo chmod 644 /var/log/snort/snort.alert.fast
